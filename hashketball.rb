74734c5b85842c96cc5f63226632906501274ffb
Chana Kupetz <chanakupetz@gmail.com>
2020-08-11 05:49:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -57,15 +57,15 @@ def game_hash
    :team_name => " ",
    :colors => [" ", " "],
    :players => [
-    :player_name
-    :number
-    :shoe
-    :points
-    :rebounds
-    :assists
-    :steals
-    :blocks
-    :slam_dunks]
+     :player_name => ,
+     :number => ,
+     :shoe => ,
+     :points => ,
+     :rebounds => ,
+     :assists => ,
+     :steals => ,
+     :blocks => ,
+     :slam_dunks => ]
 }
 }
 

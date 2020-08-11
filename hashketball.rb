b8f926875da44271c1de90efa6e1a5f1750a0121
Chana Kupetz <chanakupetz@gmail.com>
2020-08-11 05:40:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,32 @@
 def game_hash
-{:home => {},
- :away => {}
+{:home => {
+  :team_name => "Brooklyn Nests"
+  :colors => ["Black", "white"]
+  :players => [{
+    :player_name => "Alan Anderson"
+    :number => 0
+    :shoe => 16
+    :points => 22
+    :rebounds => 12
+    :assists => 12
+    :steals =>3
+    :blocks => 1
+    :slam_dunks => 1}] 
+},
+ :away => {
+   :team_name => " "
+ :colors => [" ", " "]
+ :players => [
+    :player_name
+    :number
+    :shoe
+    :points
+    :rebounds
+    :assists
+    :steals
+    :blocks
+    :slam_dunks]
+}
 }
 
 end

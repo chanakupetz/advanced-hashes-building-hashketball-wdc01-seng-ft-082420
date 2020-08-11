4959148e3087177e90195395a43a7b7fc0737245
Chana Kupetz <chanakupetz@gmail.com>
2020-08-11 05:45:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,7 +11,27 @@ def game_hash
     :assists => 12,
     :steals => 3,
     :blocks => 1,
-    :slam_dunks => 1}, {}]
+    :slam_dunks => 1}, {
+      :player_name => "Reggie Evans",
+      :number => 30, 
+      :shoe => 14, 
+      :points => 12,
+      :rebounds => 12, 
+      :assists => 12,
+      :steals => 12,
+      :blocks => 12,
+      :slam_dunks 7
+      }, {
+        :player_name => ,
+        :number => ,
+        :shoe => ,
+        :points => ,
+        :rebounds => ,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks => 
+        }]
 },
  :away => {
    :team_name => " ",

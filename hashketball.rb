98a9b48e8f2108229c613323e9b01a6f3f536385
Chana Kupetz <chanakupetz@gmail.com>
2020-08-11 05:53:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -74,7 +74,7 @@ def game_hash
        :assists => 7,
        :steals => 22,
        :blocks => 15,
-       :slam_dunks => 10 
+       :slam_dunks => 10
        }, {
          :player_name => "DeSagna Diop",
          :number => 2,
@@ -84,7 +84,7 @@ def game_hash
          :assists => 12,
          :steals => 4,
          :blocks => 5,
-         :slam_dunks => 5 
+         :slam_dunks => 5
          }, {
            :player_name => "Ben Gordon",
            :number => 8,
@@ -94,7 +94,7 @@ def game_hash
            :assists => 2,
            :steals => 1,
            :blocks => 1,
-           :slam_dunks => 0 
+           :slam_dunks => 0
            }, {
              :player_name => "Kemba Walker",
              :number => 33,
